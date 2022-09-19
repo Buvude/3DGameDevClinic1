1ee73ea505c8d5141a57759e77655ec932e0f3ab
Buvude <90856761+Buvude@users.noreply.github.com>
2022-09-19 06:21:16 -0700

    added radial sensory stuff for raptors

--- My project/Assets/Scripts/controllers/EnemyController.cs
+++ My project/Assets/Scripts/controllers/EnemyController.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     public float lookRadius;
+    private Transform target;
+    NavMeshAgent agent;
+   /* public enum state { wander, spotted};
+    public state currentState = state.wander;*/
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("Player").transform; 
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= lookRadius)
+        {
+            /*currentState = state.spotted;*/
+            agent.SetDestination(target.position);
+        }
         
     }
     private void OnDrawGizmos()
