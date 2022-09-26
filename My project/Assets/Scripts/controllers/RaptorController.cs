3d40a292881fae9c6a57bb74615551169bf5b751
dcalitri <71895943+dcalitri@users.noreply.github.com>
2022-09-26 18:36:48 -0400

    Made the raptor rotate upon death and the sound stop after dying.

--- My project/Assets/Scripts/controllers/RaptorController.cs
+++ My project/Assets/Scripts/controllers/RaptorController.cs
@@ -7,11 +7,12 @@ public class RaptorController : MonoBehaviour
     public float raptorHealth;// hits it takes for the raptor to die
     public AudioClip hitSound; // sound played when raptor takes damage
     public AudioSource raptorAudio; // source for sound
+    public bool isDead;
    
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Meat"))
+        if (other.gameObject.CompareTag("Meat") && isDead == false)
         {
             
             raptorAudio.clip = hitSound;
@@ -20,7 +21,9 @@ public class RaptorController : MonoBehaviour
 
             if (raptorHealth <= 0)
             {
-                Destroy(gameObject);
+                transform.rotation = Quaternion.Euler(new Vector3 (transform.rotation.x, transform.rotation.y + 90, transform.rotation.z));
+                isDead = true;
+
             }
         }
     }
