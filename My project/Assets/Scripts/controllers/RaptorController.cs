13d74ec51fec3aa6f7850a2228d7b03bc3a948c6
dcalitri <71895943+dcalitri@users.noreply.github.com>
2022-09-26 19:53:01 -0400

    Made a death sound and made it play when the raptor dies

--- My project/Assets/Scripts/controllers/RaptorController.cs
+++ My project/Assets/Scripts/controllers/RaptorController.cs
@@ -6,6 +6,7 @@ public class RaptorController : MonoBehaviour
 {
     public float raptorHealth;// hits it takes for the raptor to die
     public AudioClip hitSound; // sound played when raptor takes damage
+    public AudioClip deathSound; // sound played when raptor takes damage
     public AudioSource raptorAudio; // source for sound
     public bool isDead;
    
@@ -17,14 +18,17 @@ public class RaptorController : MonoBehaviour
 
             Destroy(other.gameObject);
             raptorAudio.clip = hitSound;
-            raptorAudio.PlayOneShot(hitSound, 1.0f);
             raptorHealth--;
+            if(raptorHealth >= 1)
+			{
+                raptorAudio.PlayOneShot(hitSound, 1.0f);
+            }
 
             if (raptorHealth <= 0)
             {
-                transform.rotation = Quaternion.Euler(new Vector3 (transform.rotation.x, transform.rotation.y + 90, transform.rotation.z));
                 isDead = true;
-
+                raptorAudio.PlayOneShot(deathSound, 1.0f);
+                transform.rotation = Quaternion.Euler(new Vector3 (transform.rotation.x, transform.rotation.y + 90, transform.rotation.z));
             }
         }
     }
